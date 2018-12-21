876f91d9a84beab5c5f64382a89d540aca976dc4
ashwintantri <ashwin.tantri@somaiya.edu>
2018-12-21 14:45:46 +0530

    removed unnecessary elements

--- client/src/App.js
+++ client/src/App.js
@@ -13,22 +13,22 @@ class App extends Component {
     }
   }
 
-  send = ()=>{
-    const socket = socketIOClient(this.state.endpoint);
-    socket.emit('change color', this.state.color);
-  }
 
   setColor = (color)=>{
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit('change color', this.state.color);
     this.setState({color});
   }
-  render() {
+  componentDidMount()
+  {
     const socket = socketIOClient(this.state.endpoint);
     socket.on('change color', (color) => {
       document.body.style.backgroundColor = color;
     });
+  }
+  render() {
     return (
       <div style={{textAlign:'center'}}>
-        <button onClick={()=>this.send()}>Change Color!</button>
         <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
         <button id="red" onClick={() => this.setColor('red')}>Red</button>
       </div>
